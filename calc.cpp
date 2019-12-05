9d4ada403cbd993836c1f757119828921adbdc83
vaishnavi2871995 <58411119+vaishnavi2871995@users.noreply.github.com>
2019-12-05 07:21:04 -0800

    calc.cpp
    calculator program

---
+++ calc.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std
+int main()
+{
+double num1,num2;
+cout<<"add:"<<num1+num2;
+cout<<"sub:"<<num1-num2;
+cout<<"mul:"<<num1*num2;
+cout<<"div:"<<num1/num2;
+
+return o;
+}
